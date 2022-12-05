aa1cd11c5b6c97c415e2ac4f560195c37ab2e728
CheesyPhoenix <sindre.he.moe@gmail.com>
2022-12-05 19:32:51 +0100

    typewriter effect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,23 @@
-<div class="m-auto w-max mt-10">
-	<h1 class="w-max text-4xl mt-40">Welcome to CheesyPhoenix.tk!</h1>
+<script lang="ts">
+	import Typewriter from "svelte-typewriter";
+</script>
 
-	<p class="w-max text-2xl mt-5">What would you like to do?</p>
+<Typewriter mode={"cascade"}>
+	<div class="m-auto mt-10" style="width: 600px;">
+		<h1 class="w-max text-4xl mt-40">Welcome to CheesyPhoenix.tk!</h1>
 
-	<a
-		href="/projects"
-		class="w-max text-lg mt-5 block hover:underline underline-offset-2"
-		>- Browse projects</a
-	>
+		<p class="w-max text-2xl mt-5">What would you like to do?</p>
 
-	<a
-		href="https://github.com/CheesyPhoenix"
-		class="w-max text-lg mt-5 block hover:underline underline-offset-2"
-		>- View GitHub</a
-	>
-</div>
+		<a
+			href="/projects"
+			class="w-max text-lg mt-5 block hover:underline underline-offset-2"
+			>- Browse projects</a
+		>
+
+		<a
+			href="https://github.com/CheesyPhoenix"
+			class="w-max text-lg mt-5 block hover:underline underline-offset-2"
+			>- View GitHub</a
+		>
+	</div>
+</Typewriter>
