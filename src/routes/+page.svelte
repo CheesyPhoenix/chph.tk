5b69764a7d10ef78c87ee3b572eb5eb74a9fdbf4
CheesyPhoenix <sindre.he.moe@gmail.com>
2022-12-06 17:38:14 +0100

    seo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,23 @@
 </svelte:head>
 
 <!-- SEO -->
-<h1 class="w-max text-2xl lg:text-4xl hidden">Welcome to CheesyPhoenix.tk!</h1>
+<div class="m-auto mt-10 w-[370px] lg:w-[600px] hidden">
+	<h1 class="w-max text-2xl lg:text-4xl">Welcome to CheesyPhoenix.tk!</h1>
+
+	<p class="w-max text-lg lg:text-2xl mt-5">What would you like to do?</p>
+
+	<a
+		href="/projects"
+		class="w-max text-lg mt-5 block hover:underline underline-offset-2"
+		title="View projects">- Browse projects</a
+	>
+
+	<a
+		href="https://github.com/CheesyPhoenix"
+		class="w-max text-lg mt-5 block hover:underline underline-offset-2"
+		title="View GitHub">- View GitHub</a
+	>
+</div>
 
 <div
 	class="block lg:flex w-max lg:w-screen justify-center absolute left-2 lg:left-0 top-40 h-max"
