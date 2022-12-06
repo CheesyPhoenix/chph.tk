23b73de17e365c1c10bf46e0674aeef094b0551a
CheesyPhoenix <sindre.he.moe@gmail.com>
2022-12-06 14:15:43 +0100

    projects done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,22 +2,32 @@
 	import Typewriter from "svelte-typewriter";
 </script>
 
-<Typewriter mode={"cascade"}>
-	<div class="m-auto mt-10" style="width: 600px;">
-		<h1 class="w-max text-4xl mt-40">Welcome to CheesyPhoenix.tk!</h1>
+<svelte:head>
+	<title>Home</title>
+	<meta
+		name="description"
+		content="Welcome to CheesyPhoenix.tk. What do you want to do? Browse my projects or view my GitHub page?"
+	/>
+</svelte:head>
 
-		<p class="w-max text-2xl mt-5">What would you like to do?</p>
+<div class="flex w-screen justify-center absolute left-0 top-8">
+	<Typewriter mode={"cascade"}>
+		<div class="m-auto mt-10" style="width: 600px;">
+			<h1 class="w-max text-4xl mt-40">Welcome to CheesyPhoenix.tk!</h1>
 
-		<a
-			href="/projects"
-			class="w-max text-lg mt-5 block hover:underline underline-offset-2"
-			>- Browse projects</a
-		>
+			<p class="w-max text-2xl mt-5">What would you like to do?</p>
 
-		<a
-			href="https://github.com/CheesyPhoenix"
-			class="w-max text-lg mt-5 block hover:underline underline-offset-2"
-			>- View GitHub</a
-		>
-	</div>
-</Typewriter>
+			<a
+				href="/projects"
+				class="w-max text-lg mt-5 block hover:underline underline-offset-2"
+				>- Browse projects</a
+			>
+
+			<a
+				href="https://github.com/CheesyPhoenix"
+				class="w-max text-lg mt-5 block hover:underline underline-offset-2"
+				>- View GitHub</a
+			>
+		</div>
+	</Typewriter>
+</div>
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Project from "$lib/Project.svelte";
+	import Placeholder from "$lib/betterschool-icon-512.png";
+	import { fade } from "svelte/transition";
+</script>
+
+<svelte:head>
+	<title>Projects</title>
+	<meta
+		name="description"
+		content="Here you can view and read about my various projects"
+	/>
+</svelte:head>
+
+<div out:fade>
+	<h1 class="mt-2 text-3xl mb-4">Projects:</h1>
+	<Project
+		name="BetterSchool"
+		description="BetterSchool is a better way of viewing your Visma InSchool schedule. Don't waste time logging in to the app every time you just want to check when your next class starts, use BetterSchool instead!"
+		img={Placeholder}
+		link="https://betterschool.cheesyphoenix.tk"
+	/>
+</div>
