ecbc3228338042155b551a9c44381257bdba9dbc
CheesyPhoenix <sindre.he.moe@gmail.com>
2023-01-01 17:26:26 +0100

    added VC Manager

--- src/lib/Project.svelte
+++ src/lib/Project.svelte
@@ -3,6 +3,7 @@
 	export let description: string;
 	export let link: string;
 	export let img: string;
+	export let delay: number;
 
 	import Typewriter from "svelte-typewriter";
 	import { fly } from "svelte/transition";
@@ -10,22 +11,21 @@
 
 <a
 	href={link}
-	class="flex from-gray-800 to-zinc-800 hover:to-zinc-700 duration-300 transition-all bg-gradient-to-r rounded-2xl w-[90%] max-w-[40rem] p-4 space-x-4 ml-4"
-	in:fly={{ x: 200 }}
+	class="flex from-gray-800 to-zinc-800 hover:to-zinc-700 duration-300 transition-all bg-gradient-to-r rounded-2xl w-[90%] max-w-[40rem] p-4 space-x-4 ml-4 mb-4"
+	in:fly={{ x: 200, delay }}
 >
 	<img
 		src={img}
 		alt={"Image of " + name}
 		width="128"
 		height="128"
-		class="h-[128px]"
+		class="h-[128px] rounded-[1.8rem]"
 	/>
-	<Typewriter mode="cascade">
-		<div class="min-h-[128px]">
-			<h1 class="font-bold text-lg">{name}</h1>
-			<p class="min-h-[100px] overflow-auto">
-				{description}
-			</p>
-		</div>
-	</Typewriter>
+
+	<div class="min-h-[128px]">
+		<h1 class="font-bold text-lg">{name}</h1>
+		<p class="min-h-[100px] overflow-auto">
+			{description}
+		</p>
+	</div>
 </a>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import Project from "$lib/Project.svelte";
-	import Placeholder from "$lib/betterschool-icon-512.png";
+	import BetterSchoolLogo from "$lib/betterschool-icon-512.png";
+	import VCMLogo from "$lib/VCM.png";
+
 	import { fade } from "svelte/transition";
 </script>
 
@@ -17,7 +19,15 @@
 	<Project
 		name="BetterSchool"
 		description="BetterSchool is a better way of viewing your Visma InSchool schedule. Don't waste time logging in to the app every time you just want to check when your next class starts, use BetterSchool instead!"
-		img={Placeholder}
+		img={BetterSchoolLogo}
 		link="https://betterschool.cheesyphoenix.tk"
+		delay={0}
+	/>
+	<Project
+		name="VC-Manager - Discord VC automation"
+		description="VC-Manager is a simple discord bot that manages your voice channels for you. It makes sure that there is always an availible vc and deletes unused ones, all while maintaining an orderly look."
+		img={VCMLogo}
+		link="https://github.com/CheesyPhoenix/VC-Manager"
+		delay={100}
 	/>
 </div>
