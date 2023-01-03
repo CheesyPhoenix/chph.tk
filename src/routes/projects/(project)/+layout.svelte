7fc307b5e49a0ef02674ee0c41d11fda6566b682
CheesyPhoenix <sindre.he.moe@gmail.com>
2023-01-03 20:47:08 +0100

    start on markdown sub-pages

---
+++ src/routes/projects/(project)/+layout.svelte
@@ -0,0 +1,6 @@
+<div
+	class="prose prose-invert prose-headings:text-slate-300 prose-p:text-slate-400"
+>
+	<br />
+	<slot />
+</div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,19 @@
 import preprocess from "svelte-preprocess";
 import adapter from "@sveltejs/adapter-node";
+import { mdsvex } from "mdsvex";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess({
-		postcss: true,
-	}),
+	preprocess: [
+		preprocess({
+			postcss: true,
+		}),
+		mdsvex(),
+	],
+
+	extensions: [".svelte", ".svx"],
 
 	kit: {
 		adapter: adapter({ out: "dist" }),
