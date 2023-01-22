edd0cefee437cbd9b4e1730d7867f954a818f184
CheesyPhoenix <sindre.he.moe@gmail.com>
2023-01-22 15:48:55 +0100

    add logo

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
 	import { fade } from "svelte/transition";
+	import logo from "$lib/img/logo.svg";
 </script>
 
 <header out:fade={{ duration: 0 }} class="absolute top-2 left-2">
-	<a href="/" class="inline hover:underline underline-offset-2"
-		>CheesyPhoenix.tk</a
+	<a href="/" class="inline underline-offset-2"
+		><img
+			src={logo}
+			alt="Logo"
+			class="inline w-10 hover:animate-spin-slow"
+		/></a
 	>
 
 	<p class="inline">|</p>
@@ -20,4 +25,5 @@
 		Github</a
 	>
 </header>
+<br />
 <br />
